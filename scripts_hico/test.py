bba01d5f1a72aed07a5b0164b380e019371f92a1
roy <roy881020@gmail.com>
2021-06-15 15:30:37 +0900

    object detector's annotation change from origin to quattro

---
+++ scripts_hico/test.py
@@ -0,0 +1,68 @@
+import json
+import numpy as np
+import argparse
+from random import randint
+import cv2
+import os
+import copy
+
+with open('../infos/directory.json') as fp: all_data_dir = json.load(fp)
+
+ANN_FILE_train = all_data_dir + 'Annotations_hico/train_annotations.json'
+ANN_FILE_test = all_data_dir + 'Annotations_hico/test_annotations.json'
+with open(ANN_FILE_train) as fp:
+    ANNOTATIONS_train = json.load(fp)
+with open(ANN_FILE_test) as fp:
+    ANNOTATIONS_test = json.load(fp)
+
+OBJ_PATH_train_s = all_data_dir + 'Object_Detections_hico_orig/train/'
+OBJ_PATH_test_s = all_data_dir + 'Object_Detections_hico/test/'
+
+
+for i in range(len(os.listdir(OBJ_PATH_train_s))):
+    with open(OBJ_PATH_train_s + os.listdir(OBJ_PATH_train_s)[i]) as fp:
+        origin_annotation = json.load(fp)
+
+    origin_h = origin_annotation['H']
+    origin_w = origin_annotation['W']
+    origin_detections = origin_annotation['detections']
+    origin_filename = origin_annotation['image_name']
+
+
+    for j in range(len(origin_detections)):
+
+        q2_tmp = copy.deepcopy(origin_detections[j])
+        q3_tmp = copy.deepcopy(origin_detections[j])
+        q4_tmp = copy.deepcopy(origin_detections[j])
+
+        q2_tmp['box_coords'][0] = q2_tmp['box_coords'][0]
+        q2_tmp['box_coords'][1] = q2_tmp['box_coords'][1] + origin_w
+        q2_tmp['box_coords'][2] = q2_tmp['box_coords'][2]
+        q2_tmp['box_coords'][3] = q2_tmp['box_coords'][3] + origin_w
+
+        q3_tmp['box_coords'][0] = q3_tmp['box_coords'][0] + origin_h
+        q3_tmp['box_coords'][1] = q3_tmp['box_coords'][1]
+        q3_tmp['box_coords'][2] = q3_tmp['box_coords'][2] + origin_h
+        q3_tmp['box_coords'][3] = q3_tmp['box_coords'][3]
+
+        q4_tmp['box_coords'][0] = q4_tmp['box_coords'][0] + origin_h
+        q4_tmp['box_coords'][1] = q4_tmp['box_coords'][1] + origin_w
+        q4_tmp['box_coords'][2] = q4_tmp['box_coords'][2] + origin_h
+        q4_tmp['box_coords'][3] = q4_tmp['box_coords'][3] + origin_w
+
+
+        origin_detections.append(q2_tmp)
+        origin_detections.append(q3_tmp)
+        origin_detections.append(q4_tmp)
+
+    with open(all_data_dir + 'Object_Detections_hico/train/' + origin_filename, 'w') as write_json:
+        json.dump(origin_detections, write_json)
+
+
+
+
+
+
+
+
+
